d736dabc9804b7bf39b84c79fbd2830cd80ddc1e
Tina <blindarttina@gmail.com>
2026-01-06 03:25:05 -0600

    Add splash screen and prepare logo-based launcher

--- android/app/src/main/java/com/blindart/assetsalert/MainActivity.java
+++ android/app/src/main/java/com/blindart/assetsalert/MainActivity.java
@@ -1,16 +1,20 @@
 package com.blindart.assetsalert;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         TextView tv = new TextView(this);
-        tv.setText("Assets Alert");
-        tv.setTextSize(22);
+        tv.setText("Assets Alert is running");
+        tv.setTextSize(22f);
+        tv.setPadding(40, 40, 40, 40);
+
         setContentView(tv);
     }
 }
